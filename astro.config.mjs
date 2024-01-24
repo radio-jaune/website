643989eb6913554701b3b5ac8b3069fc752674d4
Jean-Baptiste Lasselle <jean.baptiste.lasselle@gmail.com>
2024-01-24 15:49:29 +0100

    fixed build by changing tsconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import { defineConfig } from "astro/config";
-
 import preact from "@astrojs/preact";
+
 
 // https://astro.build/config
 export default defineConfig({
